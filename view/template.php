5c4fd1b00529dedd7e42918b7ec901a63fc4e711
Manu <serve.emmanuel@gmail.com>
2018-05-03 15:47:08 +0200

    Commit Creation of template of the page

---
+++ view/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TP : Mini jeu de combat</title>
+        <meta charset="utf-8" />
+        <link href="" rel="stylesheet" />
+    </head>
+        
+    <body>
+        <p>Number of characters created: <?= $numberOfCharacters ?></p>
+        <?= $content ?>
+    </body>
+</html>
